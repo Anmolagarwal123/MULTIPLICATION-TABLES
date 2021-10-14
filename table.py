aa88c36b4b9084c60f5f9f5325711d50f7ab718a
Anmolagarwal123 <92009227+Anmolagarwal123@users.noreply.github.com>
2021-10-14 21:36:51 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,11 @@
+# MULTIPLICATION TABLES
+
+print("THIS HELPS TO SHOW THE TABLE OF THE PARTICULR NUMBER")
+
+num=int(input("enter the number of your choice"))
+print("*"*15)
+multiple = 1
+while(multiple <= 10):
+    print(num,"x",multiple, "=" ,num*multiple)
+    multiple=multiple+1
+print("*"*15)
